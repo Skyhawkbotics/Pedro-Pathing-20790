b447059faabc28ed035027e87fc9f0a310096f06
trevor <trevor271c@protonmail.com>
2024-12-30 15:32:03 -0800

    auto "works". doesn't immidietally stop, tries to move but somethings wrong.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/right_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/right_auto.java
@@ -2,21 +2,202 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.sun.tools.javac.jvm.Gen;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathBuilder;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
+import org.firstinspires.ftc.teamcode.pedroPathing.GeneratedPath;
 
-@Autonomous(name = "right_Auto")
+@Autonomous(name = "right_auto", group = "AUTO")
+public class right_auto extends OpMode {
 
-public class right_auto extends LinearOpMode {
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+
+    /** This is the variable where we store the state of our auto.
+     * It is used by the pathUpdate method. */
+    private int pathState;
+    private PathChain path1;
 
 
-    @Override
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
-        new GeneratedPath();
+    /** Create and Define Poses + Paths
+     * Poses are built with three constructors: x, y, and heading (in Radians).
+     * Pedro uses 0 - 144 for x and y, with 0, 0 being on the bottom left.
+     * (For Into the Deep, this would be Blue Observation Zone (0,0) to Red Observation Zone (144,144).)
+     * Even though Pedro uses a different coordinate system than RR, you can convert any roadrunner pose by adding +72 both the x and y.
+     * This visualizer is very easy to use to find and create paths/pathchains/poses: <https://pedro-path-generator.vercel.app/>
+     * Lets assume our robot is 18 by 18 inches
+     * Lets assume the Robot is facing the human player and we want to score in the bucket */
+
+
+    /** Build the paths for the auto (adds, for example, constant/linear headings while doing paths)
+     * It is necessary to do this so that all the paths are built before the auto starts. **/
+    public void buildPaths() {
+
+
+        path1 = follower.pathBuilder()
+            .addPath(
+                    // Line 1
+                    new BezierLine(
+                            new Point(9.800, 63.300, Point.CARTESIAN),
+                            new Point(39.600, 63.300, Point.CARTESIAN)
+                    )
+            )
+                   .setConstantHeadingInterpolation(Math.toRadians(0))
+                   .addPath(
+                            // Line 2
+                            new BezierLine(
+                                    new Point(39.600, 63.300, Point.CARTESIAN),
+                                    new Point(27.300, 63.300, Point.CARTESIAN)
+                            )
+                    )
+                    .setConstantHeadingInterpolation(Math.toRadians(0))
+                    .addPath(
+                            // Line 3
+                            new BezierLine(
+                                    new Point(27.300, 63.300, Point.CARTESIAN),
+                                    new Point(27.300, 45.000, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 4
+                            new BezierLine(
+                                    new Point(27.300, 45.000, Point.CARTESIAN),
+                                    new Point(64.000, 45.000, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 5
+                            new BezierLine(
+                                    new Point(64.000, 45.000, Point.CARTESIAN),
+                                    new Point(64.000, 29.500, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 6
+                            new BezierLine(
+                                    new Point(64.000, 29.500, Point.CARTESIAN),
+                                    new Point(16.000, 29.500, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 7
+                            new BezierLine(
+                                    new Point(16.000, 29.500, Point.CARTESIAN),
+                                    new Point(64.000, 29.500, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 8
+                            new BezierLine(
+                                    new Point(64.000, 29.500, Point.CARTESIAN),
+                                    new Point(64.000, 19.000, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 9
+                            new BezierLine(
+                                    new Point(64.000, 19.000, Point.CARTESIAN),
+                                    new Point(16.000, 19.000, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 10
+                            new BezierLine(
+                                    new Point(16.000, 19.000, Point.CARTESIAN),
+                                    new Point(64.000, 19.000, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 11
+                            new BezierLine(
+                                    new Point(64.000, 19.000, Point.CARTESIAN),
+                                    new Point(64.000, 9.000, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+                    .addPath(
+                            // Line 12
+                            new BezierLine(
+                                    new Point(64.000, 9.000, Point.CARTESIAN),
+                                    new Point(16.000, 9.000, Point.CARTESIAN)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0)).build();
     }
 
+    /** This switch is called continuously and runs the pathing, at certain points, it triggers the action state.
+     * Everytime the switch changes case, it will reset the timer. (This is because of the setPathState() method)
+     * The followPath() function sets the follower to run the specific path, but does NOT wait for it to finish before moving on. */
+    public void autonomousPathUpdate() {
+        follower.followPath(path1, true);
+    }
+
+    /** These change the states of the paths and actions
+     * It will also reset the timers of the individual switches **/
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
+    @Override
+    public void loop() {
+
+        // These loop the movements of the robot
+        follower.update();
+        autonomousPathUpdate();
+
+        // Feedback to Driver Hub
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+
+    /** This method is called once at the init of the OpMode. **/
+    @Override
+    public void init() {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+
+        opmodeTimer.resetTimer();
+
+        follower = new Follower(hardwareMap);
+        buildPaths();
+    }
+
+    /** This method is called continuously after Init while waiting for "play". **/
+    @Override
+    public void init_loop() {}
+
+    /** This method is called once at the start of the OpMode.
+     * It runs all the setup actions, including building paths and starting the path system **/
+    @Override
+    public void start() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+
+    /** We do not use this because everything should automatically disable **/
+    @Override
+    public void stop() {
+    }
 }
