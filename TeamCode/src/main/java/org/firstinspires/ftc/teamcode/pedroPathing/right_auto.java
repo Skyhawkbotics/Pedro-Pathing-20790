6b094af82d24807d4cae130d80e62b48a9ca2134
trevor <trevor271c@protonmail.com>
2024-12-31 10:37:14 -0800

    add ftclib, simplify auto to try to get it to work. Still doesn't work idk why it has the same PID issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/right_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/right_auto.java
@@ -1,20 +1,13 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.sun.tools.javac.jvm.Gen;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
-import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathBuilder;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
-import org.firstinspires.ftc.teamcode.pedroPathing.GeneratedPath;
 
 @Autonomous(name = "right_auto", group = "AUTO")
 public class right_auto extends OpMode {
@@ -51,94 +44,6 @@ public class right_auto extends OpMode {
                             new Point(39.600, 63.300, Point.CARTESIAN)
                     )
             )
-                   .setConstantHeadingInterpolation(Math.toRadians(0))
-                   .addPath(
-                            // Line 2
-                            new BezierLine(
-                                    new Point(39.600, 63.300, Point.CARTESIAN),
-                                    new Point(27.300, 63.300, Point.CARTESIAN)
-                            )
-                    )
-                    .setConstantHeadingInterpolation(Math.toRadians(0))
-                    .addPath(
-                            // Line 3
-                            new BezierLine(
-                                    new Point(27.300, 63.300, Point.CARTESIAN),
-                                    new Point(27.300, 45.000, Point.CARTESIAN)
-                            )
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                    .addPath(
-                            // Line 4
-                            new BezierLine(
-                                    new Point(27.300, 45.000, Point.CARTESIAN),
-                                    new Point(64.000, 45.000, Point.CARTESIAN)
-                            )
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                    .addPath(
-                            // Line 5
-                            new BezierLine(
-                                    new Point(64.000, 45.000, Point.CARTESIAN),
-                                    new Point(64.000, 29.500, Point.CARTESIAN)
-                            )
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                    .addPath(
-                            // Line 6
-                            new BezierLine(
-                                    new Point(64.000, 29.500, Point.CARTESIAN),
-                                    new Point(16.000, 29.500, Point.CARTESIAN)
-                            )
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                    .addPath(
-                            // Line 7
-                            new BezierLine(
-                                    new Point(16.000, 29.500, Point.CARTESIAN),
-                                    new Point(64.000, 29.500, Point.CARTESIAN)
-                            )
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                    .addPath(
-                            // Line 8
-                            new BezierLine(
-                                    new Point(64.000, 29.500, Point.CARTESIAN),
-                                    new Point(64.000, 19.000, Point.CARTESIAN)
-                            )
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                    .addPath(
-                            // Line 9
-                            new BezierLine(
-                                    new Point(64.000, 19.000, Point.CARTESIAN),
-                                    new Point(16.000, 19.000, Point.CARTESIAN)
-                            )
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                    .addPath(
-                            // Line 10
-                            new BezierLine(
-                                    new Point(16.000, 19.000, Point.CARTESIAN),
-                                    new Point(64.000, 19.000, Point.CARTESIAN)
-                            )
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                    .addPath(
-                            // Line 11
-                            new BezierLine(
-                                    new Point(64.000, 19.000, Point.CARTESIAN),
-                                    new Point(64.000, 9.000, Point.CARTESIAN)
-                            )
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-                    .addPath(
-                            // Line 12
-                            new BezierLine(
-                                    new Point(64.000, 9.000, Point.CARTESIAN),
-                                    new Point(16.000, 9.000, Point.CARTESIAN)
-                            )
-                    )
                     .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0)).build();
     }
 
