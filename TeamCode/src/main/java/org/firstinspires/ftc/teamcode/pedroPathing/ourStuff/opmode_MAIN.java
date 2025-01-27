3fa5ecba29ec8b2b7bc9e74cc37e6864f3fcd0fe
loduduma <loduduma@gmail.com>
2025-01-27 12:22:14 -0800

    BLAH BLAH BALh BLAH BLAH BLAH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ourStuff/opmode_MAIN.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ourStuff/opmode_MAIN.java
@@ -219,7 +219,8 @@ public class opmode_MAIN extends OpMode {
         if (gamepad2.right_stick_y < -0.3 && (up.getCurrentPosition() < arm_upper_lim)) { //left stick -, is going up! (I think it's inverted)
             //use velocity mode to move so it doesn't we all funky with the smoothing of position mode
             up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            up.setVelocity(gamepad2.right_stick_y * -1000);
+            up.setVelocity(gamepad2.right_stick_y * 1000);
+            telemetry.addLine("trying to go up ma'am");
             up_true_target_pos = 0;
         } else if (gamepad2.right_stick_y < -0.3 && (up.getCurrentPosition() >= arm_upper_lim)) {
             up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -227,6 +228,8 @@ public class opmode_MAIN extends OpMode {
         } else if (!up_zero.isPressed() && gamepad2.right_stick_y > 0.3) { //left stick +, going down
             up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             up.setVelocity(gamepad2.right_stick_y * 1000);
+            telemetry.addLine("trying to go down ma'am");
+
             up_true_target_pos = 0;
         } else if (up_zero.isPressed() && gamepad2.right_stick_y > 0.3) { // Lower limit for up
             telemetry.addData("Lower Limit Reached", up_zero);
